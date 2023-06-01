b049d5d2fa386ab0321f60af2512fa46e64ac698
ZhaoyiLi_HekJukZaaiZyuJan <ladmon@stanford.edu>
2023-06-01 09:44:54 -0700

    [随機键]     1. 増加循環（變量惟N（numExp）） 	2. 増加隨機鍵

--- README.md
+++ README.md
@@ -35,3 +35,76 @@ Metropolis algorithm:
 $$w(a\leftarrow b) = \min{(1,e^{\beta(E_a-E_b)})}$$
 therefore satisfying the condition:
 $$\frac{w(a\leftarrow b)}{w(b\leftarrow a)}=\frac{e^{-\beta E_a}}{e^{-\beta E_b}}$$
+
+<section>
+  <h2>Variables</h2>
+<p>This program accepts command-line arguments, which can be used to customize its behavior. The available options for each subprogram are listed in the following table, along with any options that are not available. Hover your mouse over to see more details. </p>
+<table>
+  <tr>
+    <th>Option</th>
+    <th>Description</th>
+    <th>Type</th>
+    <th>Default Value</th>
+
+  </tr>
+  <tr>
+    <td>-f, --fname</td>
+    <td title="">Output filename, the default file name is in the <code>../data/</code> folder</td>
+    <td>string</td>
+    <td>N/A</td>
+  </tr>
+  <tr>
+    <td>--out</td>
+    <td>output in this directory as an .out file</td>
+    <td>bool</td>
+    <td>0</td>
+  </tr>
+  <tr>
+    <td>--waitSweep</td>
+    <td title="">number of sweeps to wit before sampling</td>
+    <td>int</td>
+    <td>10</td>
+  </tr>
+    <tr>
+    <td>--rptSweep</td>
+    <td title="">number of sweeps sampled</td>
+    <td>int</td>
+    <td>10000</td>
+  </tr>
+  <tr>
+    <td>-n</td>
+    <td>Size of Lattice</td>
+    <td>int</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td>-J</td>
+    <td>interaction strength</td>
+    <td>float between 0-1</td>
+    <td>0</td>
+  </tr>
+  <tr>
+    <td>-p</td>
+    <td title="randomness for the RBIM">randomness</td>
+    <td>float between 0-1</td>
+    <td>0</td>
+  </tr>
+  <tr>
+    <td>--tmin</td>
+    <td>minimal temperature</td>
+    <td>float</td>
+    <td>0.1</td>
+  </tr>
+  <tr>
+    <td>--tmax</td>
+    <td>maximal temperature</td>
+    <td>float</td>
+	<td>2.1</td>
+  </tr>
+  <tr>
+    <td>--nt</td>
+    <td>number of bins (data points -1)</td>
+    <td>nt</td>
+	<td>10</td>
+  </tr>
+</table>
--- cpp_version/Monte_Carlo_Ising_main.cpp
+++ cpp_version/Monte_Carlo_Ising_main.cpp
@@ -21,6 +21,7 @@ struct lattice {
         double J;
         int energy;
 
+        //Constructor for the ferro case with all 1 initial condition
         lattice(const int S[2], const  double T, const double J) {
             this->S[0] = S[0];
             this->S[1] = S[1];
@@ -32,6 +33,41 @@ struct lattice {
             this->bonds = bonds;
 
             this->energy = update_energy();
+        }
+
+        //apply random initialization of spins
+        void random_initial_condition(mt19937& rng) {
+            uniform_real_distribution<double> distribution(0.0, 1.0);
+
+            for (int i = 0; i < S[0]; i++) {
+                for (int j = 0; j < S[1]; j++) {
+                    if (distribution(rng) < 0.5) {
+                        spins[i][j] = -1;
+                    } else {
+                        spins[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        //apply random initialization of bonds
+        void random_bonds(const double p, mt19937& rng) {
+            uniform_real_distribution<double> distribution(0.0, 1.0);
+
+            for (int i = 0; i < S[0]; i++) {
+                for (int j = 0; j < S[1]; j++) {
+                    if (distribution(rng) < 0.5) {
+                        bonds[0][i][j] = -1;
+                    } else {
+                        bonds[0][i][j] = 1;
+                    }
+                    if (distribution(rng) < 0.5) {
+                        bonds[1][i][j] = -1;
+                    } else {
+                        bonds[1][i][j] = 1;
+                    }
+                }
+            }
         }
 
         int update_energy() {
@@ -64,21 +100,6 @@ struct lattice {
             return stateNumber;
         }
 
-        void random_initial_condition(int seed = 0) {
-            mt19937 rng(seed);
-            uniform_real_distribution<double> distribution(0.0, 1.0);
-
-            for (int i = 0; i < S[0]; i++) {
-                for (int j = 0; j < S[1]; j++) {
-                    if (distribution(rng) < 0.5) {
-                        spins[i][j] = -1;
-                    } else {
-                        spins[i][j] = 1;
-                    }
-                }
-            }
-        }
-
         void print() {
             for (int i = 0; i < S[0]; i++) {
                 for (int j = 0; j < S[1]; j++) {
@@ -89,7 +110,6 @@ struct lattice {
         }
 
         void make_step(mt19937& rng) {
-            
             uniform_int_distribution<int> distribution(0, S[0] - 1);
 
             int x = distribution(rng);
@@ -146,8 +166,95 @@ void simulate(const int S[2], const double T, const double J, const int waitSwee
 	}
     
     lattice testlattice(S, T, J);
-    testlattice.random_initial_condition();
+    testlattice.random_initial_condition(rng);
     testlattice.energy = testlattice.update_energy();
+
+
+    // Wait for waitSweeps
+    for (int i = 0; i < waitSweeps; i++) {
+        testlattice.make_sweep(S[0]*S[1], rng);
+    }
+
+    // Repeat for rptSweeps and sample for each sweep
+    for (int i = 0; i < rptSweeps; i++) {
+        int energy = 0, state = 0, magnetization = 0; 
+        if (i % (rptSweeps / 100) == 0) {
+            cout << ".";
+            cout.flush();
+        }
+        testlattice.make_sweep(S[0]*S[1], rng);
+
+        // Recording state, energy, and magnetization
+        energy = testlattice.energy;
+        magnetization = 0;
+        for (int j = 0; j < S[0]; j++) {
+            for (int k = 0; k < S[1]; k++) {
+                magnetization += testlattice.spins[j][k];
+            }  
+        }
+        if (track_state){
+              state = testlattice.get_state_number();
+        }
+        sum_e += energy;
+        sum_e2 += energy*energy;
+        sum_m += magnetization;
+        sum_m2 += magnetization*magnetization;
+
+        if(out != 0){
+            outfile_m << magnetization << "," << flush;
+            outfile_e << energy << ","  << flush;
+            if(track_state != 0){
+                outfile_s << state << "," << flush;
+            }
+        }
+    }
+    if(out != 0){
+        outfile_m << endl;
+        outfile_e << endl;
+        if(track_state != 0){
+            outfile_s << endl;
+        }
+        outfile_stats_m << sum_m << "," << sum_m2 << endl;
+        outfile_stats_e << sum_e << "," << sum_e2 << endl;
+    }
+    outfile_m.close();
+    outfile_s.close();
+    outfile_e.close();
+}
+
+void simulate(const int S[2], const double T, const double J, const double p, const int waitSweeps, const int rptSweeps, const bool out, const bool track_state, const string fname){
+    random_device rd;
+    mt19937 rng(rd());
+    ofstream outfile_m;
+    ofstream outfile_e;
+    ofstream outfile_s;
+    ofstream outfile_stats_m;
+    ofstream outfile_stats_e;
+
+    long long int sum_e = 0, sum_m = 0;
+    unsigned long long int sum_e2 = 0, sum_m2 = 0;
+
+
+	if(out != 0){
+		outfile_m.open(fname +"_m.out", fstream::app);
+        outfile_e.open(fname +"_e.out", fstream::app);
+        outfile_stats_m.open(fname +"_stats_m.out", fstream::app);
+        outfile_stats_e.open(fname +"_stats_e.out", fstream::app);
+		
+        outfile_m << T << "," << flush;
+        outfile_e << T << "," << flush;
+        outfile_stats_m << T << "," << flush;
+        outfile_stats_e << T << "," << flush;
+        if(track_state != 0){
+             outfile_s.open(fname +"_s.out", fstream::app);
+             outfile_s << T << "," << flush;
+        }
+	}
+    
+    lattice testlattice(S, T, J);
+    testlattice.random_initial_condition(rng);
+    testlattice.energy = testlattice.update_energy();
+    testlattice.random_bonds(p, rng);
 
 
     // Wait for waitSweeps
@@ -205,18 +312,19 @@ void simulate(const int S[2], const double T, const double J, const int waitSwee
 int main(int argc, const char *argv[]) {
     start_t = clock();
     string fname;
-    int n, waitSweep, rptSweep, nt;
-    double tmin, tmax, J;
+    int n, waitSweep, rptSweep, numExp, nt;
+    double tmin, tmax, J, p;
     bool out, track_state;
 
-    //test
-    cout << "test" << endl;
-    int test_int = 1000;
-    cout << test_int << endl;
-    mpz_t long_int;
-    mpz_set_si(long_int,test_int);
-    mpz_pow_ui(long_int, long_int,10) ;
-    cout << long_int << endl;
+    // //test mpz long int
+    // cout << "test" << endl;
+    // int test_int = 1000;
+    // cout << test_int << endl;
+    // mpz_t long_int;
+    // mpz_set_si(long_int,test_int);
+    // mpz_pow_ui(long_int, long_int,10) ;
+    // cout << long_int << endl;
+    // //end test (try to install on remote server)
 
 
     cxxopts::Options options(*argv,
@@ -227,32 +335,43 @@ int main(int argc, const char *argv[]) {
 	("fname", "filename", cxxopts::value(fname)->default_value(""))
     ("track_state", "keep track of the state as an int", cxxopts::value(track_state)->default_value("0"))
     ("out", "output in this directory as an .out file", cxxopts::value(out)->default_value("0"))
+    ("N", "number of experiments", cxxopts::value(numExp)->default_value("1"))
     ("waitSweep", "number of sweeps to wit before sampling", cxxopts::value(waitSweep)->default_value("10"))
     ("rptSweep", "number of sweeps sampled", cxxopts::value(rptSweep)->default_value("10000"))
 	("J", "interaction strength", cxxopts::value(J)->default_value("-1"))
-    ("tmin", "temperature", cxxopts::value(tmin)->default_value("0.1"))
-    ("tmax", "temperature", cxxopts::value(tmax)->default_value("2.1"))
-    ("nt", "temperature", cxxopts::value(nt)->default_value("10"))
-	("n", "size of lattice", cxxopts::value(n)->default_value("3"));
+    ("p", "randomness", cxxopts::value(p)->default_value("0"))
+    ("n", "size of lattice", cxxopts::value(n)->default_value("3"))
+    ("tmin", "minimal temperature", cxxopts::value(tmin)->default_value("0.1"))
+    ("tmax", "maximal temperature", cxxopts::value(tmax)->default_value("2.1"))
+    ("nt", "number of bins", cxxopts::value(nt)->default_value("10"));
     options.parse(argc, argv);
 
     //outputing options
-	cout << "tmin:" << tmin << ";tmax:" << tmax << endl;
-	cout << "n:" << n << endl;
-    cout << "waitSweep:" << waitSweep << ", rptSweep:" << rptSweep <<  endl;
+	cout << "tmin:" << tmin << "; tmax:" << tmax << endl;
+	cout << "n:" << n << "; p:" << p << endl;
+    cout << "waitSweep:" << waitSweep << "; rptSweep:" << rptSweep << "; numExp:" << numExp << endl;
 
     int binst = 0 == nt ? 1: nt;
 
+    //loop over experiments
     if (fname == "") {
-		fname = "../data/n=" + to_string(n) + ",T=(" + to_string(tmin).substr(0,3)+ "," +to_string(tmax).substr(0,3) + "),J=" +to_string(J).substr(0,4) + ",rptSweep=" + to_string(rptSweep);
-	}
+		    fname = "../data/n=" + to_string(n) + ",T=(" + to_string(tmin).substr(0,3)+ "," +to_string(tmax).substr(0,3) + "),J=" +to_string(J).substr(0,4) + ",p=" +to_string(p).substr(0,3) + ",rptSweep=" + to_string(rptSweep);
+    }
     cout << "fname:" << fname << endl;
-    int S[2] = {n,n};
     
-	for (int j = 0; j <= nt; j++) {
-        double t = tmin + (tmax-tmin)/binst * j;
-        simulate(S, t, J, waitSweep, rptSweep, out, track_state,  fname);
-     }   
+    for (int exp = 1; exp < numExp+1; exp++){
+        
+        int S[2] = {n,n};
+        
+        for (int j = 0; j <= nt; j++) {
+            double t = tmin + (tmax-tmin)/binst * j;
+            if (p == 0){
+                simulate(S, t, J, waitSweep, rptSweep, out, track_state,  fname + ",Exp=" + to_string(exp) );
+            } else {
+                simulate(S, t, J, p, waitSweep, rptSweep, out, track_state,  fname + ",Exp=" + to_string(exp));
+            }
+        }   
+    }
 
     cout << "t(Generation):"<< double(clock()-start_t)/CLOCKS_PER_SEC << endl; //timing
     return 0;
